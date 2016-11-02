a34a06fdca0b21281b09d06adf60c8857b13ed23
minsik moon <msmoon.business@gmail.com>
2016-11-02 23:01:19 +0900

    데이터 영역 문제로 자료구조 생성방법을 바꿈, 잘됨

--- mLineContainer/lineContainer.cpp
+++ mLineContainer/lineContainer.cpp
@@ -3,15 +3,20 @@
 lineContainer::lineContainer() {};
 //2. 데이터 setter // 0,1,7,8 //0노드의 1번 라인 인덱스는 7개 숫자를 가지고, 첫 문장의 인덱스는 8이다. 
 void lineContainer::setData(int nodeIdx, int lineIdx, int lineWordCnt, int lineFirstIdx) {
-	map<int, int> wordCntAndFirstIdx;
-	wordCntAndFirstIdx[0] = lineWordCnt; //0은 한줄에 몇 글자 들어가는지 저장 
-	wordCntAndFirstIdx[1] = lineFirstIdx; //1은 문장 첫줄의 인덱스 저장
+	//밑에 주석처리된 것처럼 하면 데이터 영역 자체가 스택에 형성되어 데이터를 잃어버리게 된다. 
+	//map<int, int> wordCntAndFirstIdx;
+	//wordCntAndFirstIdx[0] = lineWordCnt; //0은 한줄에 몇 글자 들어가는지 저장 
+	//wordCntAndFirstIdx[1] = lineFirstIdx; //1은 문장 첫줄의 인덱스 저장
+	//printf("%d, %d에 %d글자 저장했습니다. \n", nodeIdx, lineIdx, wordCntAndFirstIdx[0]);
+	//map<int, map<int, int> > lineIdxAndOther;
+	//lineIdxAndOther[lineIdx] = wordCntAndFirstIdx;
 
-	map<int, map<int, int> > lineIdxAndOther;
-	lineIdxAndOther[lineIdx] = wordCntAndFirstIdx;
-
-	container[nodeIdx] = lineIdxAndOther; //컨테이너에 저장 [0][1][0] 이런식으로
+	//container[nodeIdx] = lineIdxAndOther; //컨테이너에 저장 [0][1][0] 이런식으로
 										  //0번째 노드의 1번 인덱스 라인의 글자수, 문장 첫줄의 인덱스를 조회 할 수 있다. 
+
+	container[nodeIdx][lineIdx][0] = lineWordCnt;
+	container[nodeIdx][lineIdx][1] = lineFirstIdx;
+
 }
 
 //3. 라인의 글자수를 구한다. 
