c1df661f75c702d84a3c49ecb22fe843b164fa1d
minsik moon <msmoon.business@gmail.com>
2016-11-14 15:00:23 +0900

    비쥬얼 스튜디오용 기본 템플릿

--- winWithConsole.cpp
+++ winWithConsole.cpp
@@ -10,43 +10,19 @@
 #endif
 using namespace std;
 
-
-
-/*===========================준비물=============================*/
-
-
-
-TCHAR testStr[200] = _T("가나다라마바사 아자차카 타파하");
-
-/*==========================inner method==================================*/
-
-//1. 한 화면에 몇줄이 나오게 되나?
-//1. 몇 도막, 즉 몇 개로 나뉘어야 하는지 가르쳐주는 함수. 
-int howManySentence(LONG windowLength, LONG strLength) {
-	//strLength / windowLength 하면 한도막 사이즈가 몇이 되어야 하는지 나오겠지.
-	return (strLength / windowLength) + 1;
-}
-
-//2. 
+//전역 변수들
+HINSTANCE g_Inst;
 
 
 //////////////////////////////////////////////WIN PROC/////////////////////////////////////////////////////////////////////////////////////
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
-	//members
-	HDC hdc;
-	PAINTSTRUCT ps;
-	SIZE size;
-	static RECT rect;
-
-
-
 	switch (Message) {
 
-	
 
 
-				   //======================마지막엔 콘솔 없애주기======================== 
+
+		//======================마지막엔 콘솔 없애주기======================== 
 	case WM_DESTROY: {
 
 		PostQuitMessage(0);
@@ -66,7 +42,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) 
 /* The 'main' function of Win32 GUI programs: this is where execution starts */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	_wsetlocale(LC_ALL, _T("korean"));
-
+	g_Inst = hInstance;
 	WNDCLASSEX wc; /* A properties struct of our window */
 	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
 	MSG msg; /* A temporary location for all messages */
